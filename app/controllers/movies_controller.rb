730299034f1ba36dd3fecdc96083c332164de9c0
takerowns <abhattar@uncc.edu>
2017-10-18 14:34:05 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,29 @@
 class MoviesController < ApplicationController
 
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  def index
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+
+    if params[:ratings]
+      @movies = Movie.where(rating: params[:ratings].keys)
+    end
+
+    case params[:sort]
+    when 'title'
+      @movies = Movie.order('title ASC')
+      @title_hilite = 'hilite'
+    when 'release'
+      @movies = Movie.order('release_date ASC')
+      @release_hilite = 'hilite'
+    else
+      params[:ratings] ? @movies = Movie.where(rating: params[:ratings].keys) :
+                         @movies = Movie.all
+    end
   end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
+    @movie = Movie.find(id) # Look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
-  end
-
-  def index
-    @movies = Movie.all
   end
 
   def new
@@ -19,7 +31,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(movie_params)
+    @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -30,7 +42,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
+    @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
