3c8d79bb918e05e833b6dcbd1300f1f654b0be7e
takerowns <abhattar@uncc.edu>
2017-10-18 15:02:22 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,29 +1,43 @@
 class MoviesController < ApplicationController
 
-  def index
-    @all_ratings = ['G', 'PG', 'PG-13', 'R']
-
-    if params[:ratings]
-      @movies = Movie.where(rating: params[:ratings].keys)
-    end
-
-    case params[:sort]
-    when 'title'
-      @movies = Movie.order('title ASC')
-      @title_hilite = 'hilite'
-    when 'release'
-      @movies = Movie.order('release_date ASC')
-      @release_hilite = 'hilite'
-    else
-      params[:ratings] ? @movies = Movie.where(rating: params[:ratings].keys) :
-                         @movies = Movie.all
-    end
+  # See Section 4.5: Strong Parameters below for an explanation of this method:
+  # http://guides.rubyonrails.org/action_controller_overview.html
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # Look up movie by unique ID
+    @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering = {:title => :asc}
+      @title_header = 'hilite'
+    when 'release_date'
+      ordering = {:release_date => :asc}
+      @date_header = 'hilite'
+    end
+    
+    @all_ratings = Movie.all_ratings;
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+    
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    end
+    
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      flash.keep
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+    
+    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
   end
 
   def new
@@ -31,7 +45,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -42,7 +56,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -54,4 +68,6 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  private :movie_params
+  
 end
